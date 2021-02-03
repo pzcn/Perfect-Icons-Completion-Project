be2e79b92c9f753125f996e32318e8f8965938ae
PedroZ <sdustpedro@gmail.com>
2021-02-04 00:08:27 +0800

    fix

--- mods/Icons/mod_info.sh
+++ mods/Icons/mod_info.sh
@@ -25,8 +25,8 @@ mod_install_yes()
 	ui_print "   正在安装"
     mkdir -p $MODPATH/system/media/theme/miui_mod_icons/
     mkdir -p $MODPATH/system/media/theme/default/
-    tar -xzvf $MODPATH/mods.tar.gz
-    rm -rf $MODPATH/mods.tar.gz
+    tar -xzvf $MODPATH/mods.tar.xz
+    rm -rf $MODPATH/mods.tar.xz
     cp -r $MOD_FILES_DIR/2/* $MODPATH/system/media/theme/
     cd $MOD_FILES_DIR/1/
     $zip -m -r $MODPATH/system/media/theme/default/icons *  >/dev/null
@@ -42,8 +42,8 @@ mod_install_no()
 	ui_print "   正在安装"
     mkdir -p $MODPATH/system/media/theme/miui_mod_icons/
     mkdir -p $MODPATH/system/media/theme/default/
-    tar -xzvf $MODPATH/mods.tar.gz
-    rm -rf $MODPATH/mods.tar.gz
+    tar -xzvf $MODPATH/mods.tar.xz
+    rm -rf $MODPATH/mods.tar.xz
     cp -r $MOD_FILES_DIR/3/* $MODPATH/system/media/theme/
     cd $MOD_FILES_DIR/1/
     $zip -m -r $MODPATH/system/media/theme/default/icons *  >/dev/null
