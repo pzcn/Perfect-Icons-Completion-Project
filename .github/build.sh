54246cb1bf9ceba5054e670083d25c51dcba0202
PedroZ <sdustpedro@gmail.com>
2021-02-08 08:51:02 +0800

    Update build.sh

--- .github/build.sh
+++ .github/build.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 cd ./icons
 mkdir -p 0/layer_animating_icons/
-cp -r 4/miui_mod_icons/. 0/layer_animating_icons/
+cp -r 1/miui_mod_icons/. 0/layer_animating_icons/
 mv config.xml 0/layer_animating_icons/config.xml
 cd 0/layer_animating_icons
 DIR=`ls .`
