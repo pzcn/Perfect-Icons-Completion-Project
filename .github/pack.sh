f0aa557effde4cbb9f51b776176757185b984ab2
PedroZ <sdustpedro@gmail.com>
2021-02-12 20:24:47 +0800

    fix pack

--- .github/pack.sh
+++ .github/pack.sh
@@ -1,7 +1,9 @@
+#!/bin/bash
 XZ_OPT=-9 tar cJf icons.tar.xz ./icons
 cd themes
 mv lrone icons
 XZ_OPT=-9 tar cJf themes/lrone.tar.xz icons
+cd ..
 zip -r MIUI_Icons-${{steps.tag_version.outputs.new_tag}}.zip ./common
 zip -r MIUI_Icons-${{steps.tag_version.outputs.new_tag}}.zip ./META-INF
 zip -r MIUI_Icons-${{steps.tag_version.outputs.new_tag}}.zip ./icons.tar.xz
