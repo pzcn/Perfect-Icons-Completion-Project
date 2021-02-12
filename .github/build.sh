5be835eec952ed272d630fb1b79b49d666b5b484
PedroZ <sdustpedro@gmail.com>
2021-02-12 20:21:22 +0800

    在线下载lrone主题
    测试版 可能无法正常使用

--- .github/build.sh
+++ .github/build.sh
@@ -1,12 +1,8 @@
 #!/bin/bash
 cd icons/
 mkdir -p 0/layer_animating_icons/
-mkdir -p 4b/layer_animating_icons/
 cp -r 1/miui_mod_icons/. 0/layer_animating_icons/
-cp -r 4a/miui_mod_icons/. 4b/layer_animating_icons/
 cp config.xml 0/layer_animating_icons/config.xml
-cp config.xml 4b/layer_animating_icons/config.xml
-
 cd 0/layer_animating_icons
 DIR=`ls .`
 for dir in ${DIR};do
@@ -20,8 +16,12 @@ for dir in ${DIR};do
 		cd ..
 	fi
 done
-cd ../..
+cd ../../..
 
+cd themes/lrone
+mkdir -p 4b/layer_animating_icons/
+cp -r 4a/miui_mod_icons/. 4b/layer_animating_icons/
+cp config.xml 4b/layer_animating_icons/config.xml
 cd 4b/layer_animating_icons
 DIR=`ls .`
 for dir in ${DIR};do
@@ -35,8 +35,7 @@ for dir in ${DIR};do
 		cd ..
 	fi
 done
-cd ../..
+cd ../../../..
 
-cd ..
-rm -rf ./icons/0/layer_animating_icons/config.xml
-rm -rf ./icons/4b/layer_animating_icons/config.xml
+rm -rf icons/0/layer_animating_icons/config.xml
+rm -rf themes/lrone/4b/layer_animating_icons/config.xml
---
+++ .github/pack.sh
@@ -0,0 +1,8 @@
+XZ_OPT=-9 tar cJf icons.tar.xz ./icons
+cd themes
+mv lrone icons
+XZ_OPT=-9 tar cJf themes/lrone.tar.xz icons
+zip -r MIUI_Icons-${{steps.tag_version.outputs.new_tag}}.zip ./common
+zip -r MIUI_Icons-${{steps.tag_version.outputs.new_tag}}.zip ./META-INF
+zip -r MIUI_Icons-${{steps.tag_version.outputs.new_tag}}.zip ./icons.tar.xz
+zip -r MIUI_Icons-${{steps.tag_version.outputs.new_tag}}.zip ./module.prop
