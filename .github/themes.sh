a89240489087c22b32c5d674b4601558dadeb6a9
PedroZ <sdustpedro@gmail.com>
2021-02-14 21:38:59 +0800

    [themes] 增加luck7 测试版 可能不能用

--- .github/themes.sh
+++ .github/themes.sh
@@ -47,6 +47,30 @@ mv lrone icons
 XZ_OPT=-9 tar cJf lrone.tar.xz icons
 rm -rf icons
 
+cd luck7
+mkdir -p luck71/layer_animating_icons/
+cp -r luck7_icons/miui_mod_icons/. luck71/layer_animating_icons/
+cp config.xml luck71/layer_animating_icons/config.xml
+cd luck71/layer_animating_icons
+DIR=`ls .`
+for dir in ${DIR};do
+	if [ -d ${dir} ];then
+		cp config.xml ${dir}/config.xml
+		cd ${dir}
+		echo ${dir}
+		mkdir -p 1/quiet
+        mkdir -p 0/quiet
+        mv 1.png 1/quiet/quietImage.png
+        mv 0.png 0/quiet/quietImage.png
+		cd ..
+	fi
+done
+cd ../../..
+rm -rf ./luck7/luck71/layer_animating_icons/config.xml
+mv luck7 icons
+XZ_OPT=-9 tar cJf luck7.tar.xz icons
+rm -rf icons
+
 cd ..
 mkdir output
 mv themes/*.tar.xz output
