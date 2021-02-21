b57223c5832942e8bcd89ec115a44416f6fe4e07
PedroZ <sdustpedro@gmail.com>
2021-02-21 17:29:20 +0800

    修复mtz

--- .github/mtz.sh
+++ .github/mtz.sh
@@ -5,5 +5,7 @@ cp -rf icons/MIUIdefault/default/icons.zip ./
 cp -rf icons/MIUIdefault/miui_mod_icons/* temp/res/drawable-xxhdpi/
 zip -m -r -9 ./icons.zip ./temp/*  >/dev/null
 mv ./icons.zip ./mtz/icons
-zip -m -r -9 ./mtz/MIUI_Icons.zip ./mtz/icons  >/dev/null
+cd mtz
+zip -m -r -9 ./mtz/MIUI_Icons.zip icons  >/dev/null
+cd ..
 cp -rf ./mtz/MIUI_Icons.zip ./MIUI_Icons_CI_latest.mtz
