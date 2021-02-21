36dafe99fbc193e1021bb5ef7fcd09824f14fe00
PedroZ <sdustpedro@gmail.com>
2021-02-21 17:16:07 +0800

    Update mtz.sh

--- .github/mtz.sh
+++ .github/mtz.sh
@@ -3,7 +3,7 @@ mkdir -p temp/res/drawable-xxhdpi/
 cp -rf icons/icons/miui_mod_icons/* temp/res/drawable-xxhdpi/
 cp -rf icons/MIUIdefault/default/icons.zip ./
 cp -rf icons/MIUIdefault/miui_mod_icons/* temp/res/drawable-xxhdpi/
-zip -m -r ./icons.zip ./temp/*  >/dev/null
+zip -m -r -9 ./icons.zip ./temp/*  >/dev/null
 mv ./icons.zip ./mtz/icons
-zip -m -r ./mtz/MIUI_Icons.zip ./mtz/icons  >/dev/null
+zip -m -r -9 ./mtz/MIUI_Icons.zip ./mtz/icons  >/dev/null
 cp -rf ./mtz/MIUI_Icons.zip ./MIUI_Icons_CI_latest.mtz
