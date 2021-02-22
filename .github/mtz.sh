bf199dfad58f5a7aa3f9775883ad7d09a24de25b
PedroZ <sdustpedro@gmail.com>
2021-02-22 17:15:13 +0800

    [skip mtz] 测试版 尚未完成

--- .github/mtz.sh
+++ .github/mtz.sh
@@ -4,10 +4,10 @@ cp -rf icons/icons/miui_mod_icons/* temp/res/drawable-xxhdpi/
 cp -rf icons/MIUIdefault/miui_mod_icons/* temp/res/drawable-xxhdpi/
 cp -rf icons/MIUIdefault/default/icons.zip ./temp
 cd temp
-zip -m -r -9 icons.zip res  >/dev/null
+zip -r -9 icons.zip res  >/dev/null
 cd ..
 mv ./temp/icons.zip ./mtz/icons
 cd mtz
-zip -m -r -9 MIUI_Icons.zip icons  >/dev/null
+zip -r -9 MIUI_Icons.zip icons  >/dev/null
 cd ..
 cp -rf ./mtz/MIUI_Icons.zip ./MIUI_Icons_CI_latest.mtz
