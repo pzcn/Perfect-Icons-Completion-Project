1924ddd6337d56201dfb8f88ef10dcb330029efb
PedroZ <sdustpedro@gmail.com>
2021-02-22 22:49:36 +0800

    [themes]

--- .github/themes.sh
+++ .github/themes.sh
@@ -11,6 +11,6 @@ cd themes/luck7
 XZ_OPT=-9 tar cJf luck7.tar.xz *
 cd ../..
 
-cp -rf themes/explore/*.tar.xz ./output
-cp -rf themes/lrone/*.tar.xz ./output
-cp -rf themes/luck7/*.tar.xz ./output
+cp -rf ./themes/explore/explore.tar.xz ./output
+cp -rf ./themes/lrone/lrone.tar.xz ./output
+cp -rf ./themes/luck7/luck7.tar.xz ./output
