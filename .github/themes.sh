a7c3445644f65fdeafbd9311a868d0134d442283
PedroZ <sdustpedro@gmail.com>
2021-03-21 14:21:42 +0800

    新增
    Mastiff Li 添加 vivo互传 小米白噪音 VOEZ
    
    （测试）新增Flyme9图标，除日历低仿，其他基本一致

--- .github/themes.sh
+++ .github/themes.sh
@@ -15,8 +15,13 @@ cd themes/perfectcurve
 XZ_OPT=-9 tar cJf perfectcurve.tar.xz *
 cd ../..
 
+cd themes/flyme9
+XZ_OPT=-9 tar cJf flyme9.tar.xz *
+cd ../..
+
 mkdir output
 cp -rf ./themes/explore/explore.tar.xz ./output
 cp -rf ./themes/lrone/lrone.tar.xz ./output
 cp -rf ./themes/luck7/luck7.tar.xz ./output
 cp -rf ./themes/perfectcurve/perfectcurve.tar.xz ./output
+cp -rf ./themes/flyme9/flyme9.tar.xz ./output
