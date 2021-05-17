2199e22de2efb7e9e59f7118760824acf86795c3
pzcn <sdustpedro@gmail.com>
2021-05-17 23:07:16 +0800

    Update themes.sh

--- .github/themes.sh
+++ .github/themes.sh
@@ -34,26 +34,3 @@ cp -rf ./themes/lrone/lrone.tar.xz ./output
 cp -rf ./themes/luck7/luck7.tar.xz ./output
 cp -rf ./themes/perfectcurve/perfectcurve.tar.xz ./output
 cp -rf ./themes/flyme9/flyme9.tar.xz ./output
-
-cd ./output
-:> icons.ini
-:> default.ini
-:> lrone.ini
-:> explore.ini
-:> luck7.ini
-:> perfectcurve.ini
-:> flyme9.ini
-echo "file_size=`ls -l ./icons.tar.xz | awk '{print $5}'`" >> ./icons.ini
-echo "file_size=`ls -l ./default.tar.xz | awk '{print $5}'`" >> ./default.ini
-echo "file_size=`ls -l ./explore.tar.xz | awk '{print $5}'`" >> ./explore.ini
-echo "file_size=`ls -l ./luck7.tar.xz | awk '{print $5}'`" >> ./luck7.ini
-echo "file_size=`ls -l ./lrone.tar.xz | awk '{print $5}'`" >> ./lrone.ini
-echo "file_size=`ls -l ./perfectcurve.tar.xz | awk '{print $5}'`" >> ./perfectcurve.ini
-echo "file_size=`ls -l ./flyme9.tar.xz | awk '{print $5}'`" >> ./flyme9.ini
-echo "md5=`md5sum ./icons.tar.xz|cut -d ' ' -f1`" >> ./icons.ini
-echo "md5=`md5sum ./default.tar.xz|cut -d ' ' -f1 " >> ./default.ini
-echo "md5=`md5sum ./explore.tar.xz|cut -d ' ' -f1 " >> ./explore.ini
-echo "md5=`md5sum ./luck7.tar.xz|cut -d ' ' -f1 " >> ./luck7.ini
-echo "md5=`md5sum ./lrone.tar.xz|cut -d ' ' -f1 " >> ./lrone.ini
-echo "md5=`md5sum ./perfectcurve.tar.xz|cut -d ' ' -f1 " >> ./perfectcurve.ini
-echo "md5=`md5sum ./flyme9.tar.xz|cut -d ' ' -f1 " >> ./flyme9.ini
