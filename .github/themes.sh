024fee39f8b62158e41b4813ec32d13450b626f1
pzcn <sdustpedro@gmail.com>
2021-05-18 00:58:54 +0800

    Update themes.sh

--- .github/themes.sh
+++ .github/themes.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
-
-XZ_OPT=-9 tar cJf icons.tar.xz icons
-
+mkdir -p ./temp/res/drawable-xxhdpi/
+cp -rf icons/* ./temp/res/drawable-xxhdpi/
+cd temp
+zip -r -9 icons.zip *  >/dev/null
+XZ_OPT=-9 tar cJf icons.tar.xz icons.zip
+cd ..
 cd addition/default
 XZ_OPT=-9 tar cJf default.tar.xz *
 cd ../..
@@ -27,7 +30,7 @@ XZ_OPT=-9 tar cJf flyme9.tar.xz *
 cd ../..
 
 mkdir output
-cp -rf ./icons.tar.xz ./output
+cp -rf ./temp/icons.tar.xz ./output
 cp -rf ./addition/default/default.tar.xz ./output
 cp -rf ./themes/explore/explore.tar.xz ./output
 cp -rf ./themes/lrone/lrone.tar.xz ./output
